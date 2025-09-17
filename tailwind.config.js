0aa2ecbb34114b1312b8fd1b2c8717eccfbf2ac6
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-09-17 20:07:21 +0100

    added conventional color to the the header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"], // Include all HTML files in the root
+  content: ["./*.html", "./src/**/*.html"], // Include all HTML files in the root
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#f0e2fe',
+        accent:'#2e5ef7',
+        accentLight:'#e467ee',
+        textPrimary: '#e0befe',
+      }
+    },
   },
   plugins: [],
 };
